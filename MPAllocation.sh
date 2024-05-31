4b40a45463e9130dd5d5ff2c57bb784a37d848e5
bbwill24 <65236789+bbwill24@users.noreply.github.com>
2024-05-31 18:35:18 -0400

    Update MPAllocation.sh

--- MPAllocation.sh
+++ MPAllocation.sh
@@ -6,26 +6,22 @@ MP="/mnt/location"
 Part_Number="number associated with partition being resized" 
 
 #Unmount the file system as it is not a root drive
-
 umount $MP
 
 # resize the file system 
-
 growpart /dev/$Disk_Label $Part_Number 
 
 #Run file system check to ensure drive is in optimal condition 
-
 e2fsck -f /dev/$Disk_Label
 
 #Resize the file system to fill the logical volume
-
 resize2fs /dev/$Disk_Label
 
 # Remount the file system 
 
 mount /dev/$Disk_Label $MP
 
+echo "The volume allocation has successfully completed." 
 
-# confirm mount point has been properly configured
-
-df -h 
+# Kickoff: "chmod +x MPAllocation.sh" - makes file executable 
+# Execute: ./MPAllocation.sh
