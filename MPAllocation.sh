9d6bfe11ecc5cffde2c874c4ede9ed26064b0089
bbwill24 <65236789+bbwill24@users.noreply.github.com>
2024-06-02 16:57:04 -0400

    Update MPAllocation.sh

--- MPAllocation.sh
+++ MPAllocation.sh
@@ -2,14 +2,13 @@
 
 # Variables - Use this in place of profuction variables
 Disk_Label="Name of disk being expanded" 
-MP="/mnt/location"
-Part_Number="number associated with partition being resized" 
+MP="/mnt/location" 
 
 #Unmount the file system as it is not the root drive
 umount $MP
 
 # resize the file system 
-growpart /dev/$Disk_Label $Part_Number 
+growpart /dev/$Disk_Label 
 
 #Run file system check to ensure drive is in optimal condition 
 e2fsck -f /dev/$Disk_Label
